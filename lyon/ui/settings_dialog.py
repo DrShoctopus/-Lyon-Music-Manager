6f006c02f404414f71c0eaf8d9011811ed69ec00
DrShoctopus <impressiverisk2@gmail.com>
2026-05-17 16:38:58 -0400

    Update settings_dialog.py

--- lyon/ui/settings_dialog.py
+++ lyon/ui/settings_dialog.py
@@ -271,8 +271,8 @@ class SettingsDialog(QDialog):
         layout.addSpacing(12)
 
         desc = QLabel(
-            "Sea Lyon is a music library manager, CD ripper, and player\n"
-            "for Windows, macOS, and Linux."
+            "Sea Lyon is a music library manager, CD ripper, and audio/video player\n"
+            "for Windows,Coming Soon to macOS"
         )
         desc.setWordWrap(True)
         layout.addWidget(desc)
